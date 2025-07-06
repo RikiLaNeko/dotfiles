e6cbee275cc3c3fa09d4e05b0ecad8f48b98db3f
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2025-07-06 22:53:03 +0200

    updated lazy, new lockscreen by sway

--- .config/wofi/wofi.sh
+++ .config/wofi/wofi.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+<<<<<<< HEAD
 wofi \
   --conf "$HOME/.config/wofi/config" \
   --style "$HOME/.config/wofi/src/mocha/style.css" \
   --show drun
+=======
+CONFIG="$HOME/.config/wofi/config"
+STYLE="$HOME/.config/wofi/src/mocha/style.css"
+
+if [[ ! $(pidof wofi) ]]; then
+  wofi --conf "${CONFIG}" --style "${STYLE}" --show drun &
+else
+  pkill wofi
+fi
+>>>>>>> 17994da (Added in wofi)
