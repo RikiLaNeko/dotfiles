ef7ab838740ac20655e5f46666b973ecd3d55b6c
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2025-07-20 09:53:07 +0200

    better wofi config

--- .config/wofi/wofi.sh
+++ .config/wofi/wofi.sh
@@ -2,9 +2,10 @@
 
 CONFIG="$HOME/.config/wofi/config"
 STYLE="$HOME/.config/wofi/src/mocha/style.css"
+PROMPT="Recherche le JaaJ"
 
 if [[ ! $(pidof wofi) ]]; then
-  wofi --conf "${CONFIG}" --style "${STYLE}" --show drun &
+  wofi --conf "${CONFIG}" --style "${STYLE}" --insensitive --prompt "${PROMPT}" --show drun &
 else
   pkill wofi
 fi
